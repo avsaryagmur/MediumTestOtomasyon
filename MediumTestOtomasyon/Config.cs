c26869d9d869e5ba30b1315829bfd74354a487bd
yagmuravsar <yagmur.avsar@deliveryhero.com>
2023-03-10 01:49:16 +0100

    config class

---
+++ MediumTestOtomasyon/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MediumTestOtomasyon
+{
+    public static class Config
+    {
+        public static string MediumTestUrl = "https://www.yemeksepeti.com/istanbul";
+        public static string OtomasyonScreenshotDirectory = AppDomain.CurrentDomain.BaseDirectory + @"\OtomasyonScreenShots";//Projenin bulunduğu dosya yolunun altına yeni bir klosör yolu ekler.
+
+    }
+}
+
